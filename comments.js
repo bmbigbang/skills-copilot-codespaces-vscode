fb0b113660c2cb54504b9f5e72947e5632b0ff06
Ardavan Shafiee <bm.bigbang@gmail.com>
2023-12-20 15:08:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController')
+
+router.get('/', commentController.comment_list);
